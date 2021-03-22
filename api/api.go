e32d0fdbdcc33fd68f112ae6f0ba7ac17a5118c3
CarlosRoGuerra <crrobg2005@yahoo.com.br>
2021-03-22 10:05:11 -0300

    add test case to create

--- api/api.go
+++ api/api.go
@@ -17,7 +17,8 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	var user types.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		fmt.Println(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 	for _, item := range users {
 		if item.Id == user.Id {
@@ -29,81 +30,73 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// func createUser(w http.ResponseWriter, r *http.Request) {
-// 	users := []types.User{
-// 		{Id: "123", Name: "seila", Password: "456"},
-// 		{Id: "456", Name: "test", Password: "678"},
-// 	}
-// 	var user types.User
-// 	err := json.NewDecoder(r.Body).Decode(&user)
-// 	if err != nil {
-// 		fmt.Println(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-// 	fmt.Printf("%v", user)
+func createUser(w http.ResponseWriter, r *http.Request) {
+	// users := []types.User{
+	// 	{Id: "123", Name: "seila", Password: "456"},
+	// 	{Id: "456", Name: "test", Password: "678"},
+	// }
+	var user types.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Println(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+	w.WriteHeader(http.StatusCreated)
+}
 
-// 	w.WriteHeader(http.StatusCreated)
-// }
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	users := []types.User{
+		{Id: "123", Name: "seila", Password: "456"},
+	}
+	var user types.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Println(w, err.Error(), http.StatusBadRequest)
+	}
+	for index, item := range users {
+		if item.Id == user.Id {
+			users = append(users[:index], users[index+1:]...)
 
-// func updateUser(w http.ResponseWriter, r *http.Request) {
-// 	users := []types.User{
-// 		{Id: "123", Name: "seila", Password: "456"},
-// 		{Id: "456", Name: "test", Password: "678"},
-// 	}
-// 	var tuser types.User
-// 	err := json.NewDecoder(r.Body).Decode(&tuser)
-// 	for index, item := range users {
-// 		if item.Id == tuser.Id {
-// 			users = append(users[:index], users[index+1:]...)
-// 			_ = json.NewDecoder(r.Body).Decode(&tuser)
-// 			users = append(users, tuser)
-// 		}
-// 		if err != nil {
-// 			fmt.Println(w, err.Error(), http.StatusBadRequest)
-// 			return
-// 		}
-// 	}
-// 	if tuser.Name == item.Name {
-// 		fmt.Printf("Usuario Atualizado!")
-// 		w.WriteHeader(http.StatusOK)
-// 		json.NewEncoder(w).Encode(tuser)
-// 		return
-// 	} else {
-// 		fmt.Printf("Error")
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
-// }
+		}
+		json.NewEncoder(w).Encode(users)
+	}
+	var newuser types.User
+	_ = json.NewDecoder(r.Body).Decode(&newuser)
+	newuser.Id = user.Id
+	newuser.Name = user.Name
+	newuser.Password = user.Password
+	users = append(users, newuser)
+	json.NewEncoder(w).Encode(users)
+	w.WriteHeader(http.StatusOK)
 
-// func deleteUser(w http.ResponseWriter, r *http.Request) {
-// 	var duser types.User
-// 	users = append(users, types.User{Id: "1", Name: "test-tudo", Password: "12456"})
-// 	//params := mux.Vars(r)
-// 	err := json.NewDecoder(r.Body).Decode(&duser)
-// 	for index, item := range users {
-// 		if item.Id == duser.Id {
-// 			users = append(users[:index], users[index+1:]...)
-// 			break
-// 		}
-// 	}
-// 	json.NewEncoder(w).Encode(duser)
-// 	if err != nil {
-// 		fmt.Println(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-// 	fmt.Printf("%v :", users)
-// 	if users == nil {
-// 		fmt.Printf("User Deleted!")
-// 	}
-// 	w.WriteHeader(301)
-// }
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	users := []types.User{
+		{Id: "123", Name: "seila", Password: "456"},
+	}
+	var user types.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Println(w, err.Error(), http.StatusBadRequest)
+	}
+	for index, item := range users {
+		if item.Id == user.Id {
+			users = append(users[:index], users[index+1:]...)
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+	http.Error(w, "user not found", http.StatusNotFound)
+}
 
 func New() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/users", getUser).Methods("GET")
-	//router.HandleFunc("/users", createUser).Methods("POST")
-	//router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
-	//router.HandleFunc("/users", deleteUser).Methods("DELETE")
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/users", deleteUser).Methods("DELETE")
 
 	return router
 }
--- api/api_test.go
+++ api/api_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,12 +17,85 @@ func TestGetUser(t *testing.T) {
 	defer ts.Close()
 	var tt = []struct {
 		name         string
-		userId       string
+		body         *bytes.Buffer
 		expectedUser types.User
 		assertion    func(*testing.T, *http.Response, types.User, error)
 	}{
 		{
 			name: "when user is found",
+			expectedUser: types.User{
+				Id:       "123",
+				Name:     "seila",
+				Password: "456",
+			},
+			body: func() *bytes.Buffer {
+				user := types.User{
+					Id: "123",
+				}
+				bbytes, _ := json.Marshal(user)
+				return bytes.NewBuffer(bbytes)
+			}(),
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, err error) {
+				assert.NoError(t, err)
+				var user types.User
+				err = json.NewDecoder(resp.Body).Decode(&user)
+				assert.NoError(t, err)
+				assert.Equal(t, expectedUser, user)
+				assert.Equal(t, 200, resp.StatusCode)
+			},
+		},
+		{
+			name: "when user not found",
+			body: func() *bytes.Buffer {
+				user := types.User{
+					Id: "000",
+				}
+				bbytes, _ := json.Marshal(user)
+				return bytes.NewBuffer(bbytes)
+			}(),
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, err error) {
+				assert.NoError(t, err)
+				var user types.User
+				err = json.NewDecoder(resp.Body).Decode(&user)
+				assert.Error(t, err, "EOF")
+				assert.Equal(t, expectedUser, user)
+				assert.Equal(t, 404, resp.StatusCode)
+			},
+		},
+		{
+			name: "when server receive gibberish",
+			body: func() *bytes.Buffer {
+				user := "ggff"
+				bbytes, _ := json.Marshal(user)
+				return bytes.NewBuffer(bbytes)
+			}(),
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, e error) {
+				assert.NoError(t, e)
+				body, err := ioutil.ReadAll(resp.Body)
+				assert.NoError(t, err)
+				assert.Equal(t, "json: cannot unmarshal string into Go value of type types.User\n", string(body))
+				assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+			},
+		},
+	}
+	for _, testcase := range tt {
+		resp, err := ts.Client().Post(ts.URL, "application/json", testcase.body)
+		testcase.assertion(t, resp, testcase.expectedUser, err)
+
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(updateUser))
+	defer ts.Close()
+	var tt = []struct {
+		name         string
+		userId       string
+		expectedUser types.User
+		assertion    func(*testing.T, *http.Response, types.User, error)
+	}{
+		{
+			name: "when user is updated",
 			expectedUser: types.User{
 				Id:       "123",
 				Name:     "seila",
@@ -37,9 +111,6 @@ func TestGetUser(t *testing.T) {
 				assert.Equal(t, 200, resp.StatusCode)
 			},
 		},
-		{
-			name: "when user not found",
-		},
 	}
 	for _, testcase := range tt {
 		user := types.User{
@@ -54,46 +125,107 @@ func TestGetUser(t *testing.T) {
 	}
 }
 
-// func TestUpdateUser(t *testing.T) {
+func TestCreateUser(t *testing.T) {
+	tp := httptest.NewServer(http.HandlerFunc(createUser))
+	defer tp.Close()
+	var tt = []struct {
+		name         string
+		userId       string
+		expectedUser types.User
+		assertion    func(*testing.T, *http.Response, types.User, error)
+	}{
+		{
+			name: "when user is create",
+			expectedUser: types.User{
+				Id:       "123",
+				Name:     "clemente",
+				Password: "456",
+			},
+			userId: "123",
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, err error) {
+				assert.NoError(t, err)
+				var user types.User
+				err = json.NewDecoder(resp.Body).Decode(&user)
+				assert.NoError(t, err)
+				assert.Equal(t, expectedUser, user)
+				assert.Equal(t, 200, resp.StatusCode)
+			},
+		},
+		{
+			name:         "when user is empty",
+			expectedUser: types.User{},
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, err error) {
+				assert.NoError(t, err)
+				var user types.User
+				err = json.NewDecoder(resp.Body).Decode(&user)
+				assert.Error(t, err)
+				assert.Equal(t, expectedUser, user)
+				assert.Equal(t, 404, resp.StatusCode)
+			},
+		},
+	}
+	for _, testcase := range tt {
+		bbytes, err := json.Marshal(testcase.expectedUser)
+		assert.NoError(t, err)
+		bb := bytes.NewBuffer(bbytes)
+		resp, err := tp.Client().Post(tp.URL, "application/json", bb)
+		assert.Equal(t, 200, resp.StatusCode)
+		testcase.assertion(t, resp, testcase.expectedUser, err)
+	}
+}
 
-// 	tp := httptest.NewServer(http.HandlerFunc(updateUser))
-// 	for _, testcase := range tt {
-// 		bbytes, err := json.Marshal(testcase.user)
-// 		assert.NoError(t, err)
-// 		bb := bytes.NewBuffer(bbytes)
-// 		gs, err := tp.Client().Post(tp.URL, "application/json", bb)
-// 		response, err := ioutil.ReadAll(gs.Body)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		var jsond []types.User
-// 		err = json.Unmarshal(response, &jsond)
-// 		fmt.Println(jsond)
-// 		assert.Equal(t, 200, gs.StatusCode)
-// 	}
-// }
+func TestDeleteUser(t *testing.T) {
+	tp := httptest.NewServer(http.HandlerFunc(deleteUser))
+	defer tp.Close()
+	var tt = []struct {
+		name         string
+		userId       string
+		body         *bytes.Buffer
+		expectedUser types.User
+		assertion    func(*testing.T, *http.Response, types.User, error)
+	}{
+		{
+			name:         "when user is successfully deleted",
+			expectedUser: types.User{},
+			body: func() *bytes.Buffer {
+				user := types.User{
+					Id: "123",
+				}
+				bbytes, _ := json.Marshal(user)
+				return bytes.NewBuffer(bbytes)
+			}(),
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, err error) {
+				assert.NoError(t, err)
+				var user types.User
+				err = json.NewDecoder(resp.Body).Decode(&user)
+				assert.Error(t, err)
+				assert.Equal(t, expectedUser, user)
+				assert.Equal(t, 200, resp.StatusCode)
+			},
+		},
+		{
+			name:         "when user not deleted",
+			expectedUser: types.User{},
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, err error) {
+				assert.NoError(t, err)
+				var user types.User
+				err = json.NewDecoder(resp.Body).Decode(&user)
+				assert.NoError(t, err)
+				assert.Equal(t, expectedUser, user)
+				assert.Equal(t, 404, resp.StatusCode)
+			},
+		},
+		{
+			name: "when server receive gibberish",
+			body: nil,
+			assertion: func(t *testing.T, resp *http.Response, expectedUser types.User, err error) {
+				assert.NoError(t, err)
+			},
+		},
+	}
+	for _, testcase := range tt {
+		resp, err := tp.Client().Post(tp.URL, "application/json", testcase.body)
+		testcase.assertion(t, resp, testcase.expectedUser, err)
 
-// func TestGetUser(t *testing.T) {
-// 	tp := httptest.NewServer(http.HandlerFunc(getUser))
-// 	for _, testcase := range tt {
-// 		bbytes, err := json.Marshal(testcase.user)
-// 		assert.NoError(t, err)
-// 		bb := bytes.NewBuffer(bbytes)
-// 		gs, err := tp.Client().Post(tp.URL, "application/json", bb)
-// 		assert.Equal(t, 200, gs.StatusCode)
-// 	}
-// }
-// func TestDeleteUser(t *testing.T) {
-
-// 	tp := httptest.NewServer(http.HandlerFunc(updateUser))
-// 	for _, testcase := range tt {
-// 		bbytes, err := json.Marshal(testcase.user)
-// 		assert.NoError(t, err)
-// 		bb := bytes.NewBuffer(bbytes)
-// 		gs, err := tp.Client().Post(tp.URL, "application/json", bb)
-// 		// if gs.StatusCode != 301 {
-// 		// 	t.Errorf("Returned wrong status code: got %v want %v", gs.StatusCode, 301)
-// 		// }
-// 		assert.Equal(t, 301, gs.StatusCode)
-// 	}
-// }
+	}
+}
